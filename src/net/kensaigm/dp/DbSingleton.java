9d2c9923c2e0eb0df7b563be2681919e982a3e0b
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-07 16:33:01 -0700

    Modified DbSingleton to add getConnection for derby.

--- src/net/kensaigm/dp/DbSingleton.java
+++ src/net/kensaigm/dp/DbSingleton.java
@@ -1,11 +1,27 @@
 package net.kensaigm.dp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSingleton {
 
     private static volatile DbSingleton instance = null;
+    private static volatile conn = null;
 
     private DbSingleton(){
-        if(instance != null) {
+
+        try {
+            DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        if(conn != null) {
+            throw new RuntimeException("Use getConnection() method to create.");
+        }
+
+        if(instance != null){
             throw new RuntimeException("Use getInstance() method to create.");
         }
     }
@@ -21,5 +37,23 @@ public class DbSingleton {
         }
 
         return instance;
+    }
+
+    public Connection getConnection(){
+        if(conn == null){
+            synchronized (DbSingleton.class){
+                if (conn == null) {
+                    try {
+                        String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+
+                        conn = DriverManager.getConnection(dbUrl);
+                    } catch (SQLException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        return conn;
     }
 }
