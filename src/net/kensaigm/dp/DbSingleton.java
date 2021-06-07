56229caa0e0bd7e838a654b705fd30941192ed2b
Kensaigm <Kensaigm@users.noreply.github.com>
2021-06-07 15:57:39 -0700

    Modified DbSingleton to allow for lazy instantiation.

--- src/net/kensaigm/dp/DbSingleton.java
+++ src/net/kensaigm/dp/DbSingleton.java
@@ -2,11 +2,16 @@ package net.kensaigm.dp;
 
 public class DbSingleton {
 
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance = null;
 
     private DbSingleton(){}
 
     public static DbSingleton getInstance() {
+
+        if (instance == null){
+            instance = new DbSingleton();
+        }
+
         return instance;
     }
 }
